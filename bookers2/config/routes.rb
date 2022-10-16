492a05d955289a22302183097ecba25187b50971
EC2 Default User <ec2-user@ip-172-31-90-2.ec2.internal>
2022-10-16 08:46:17 +0000

    課題３追加

---
+++ bookers2/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  resources :books
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/home/about' => 'homes#about', as: 'about'
+
+  resources :users
+
+  root to: 'homes#top'
+end
